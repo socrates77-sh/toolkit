9d52bb8b30678755f0ab82b2998edcee028bf045
socrates77 <zhwenrong@sina.com>
2018-09-15 09:57:48 +0800

    done hhg_online.py v1.0 @home
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- hhg_online.py
+++ hhg_online.py
@@ -1,5 +1,5 @@
 # history:
-# 2018/09/14  v1.0  initial
+# 2018/09/14  v1.5  initial
 
 
 import csv
@@ -9,16 +9,21 @@ import json
 import msvcrt
 import os
 import re
+import shutil
 import sys
 
-import requests
 import filetype
+import requests
 
 VERSION = '1.0'
 URL_TDIS_TOP = 'http://online.hhgrace.com/web/get_treeList_ztree.tdis'
 URL = 'http://online.hhgrace.com/web/get_docList_search.tdis?treeNodeId=%s&userId=2c9e498b48a7386f0148ac31c83f0108&description=0.11um/F011Q7E8/Design%20Rule/Platform/'
-HEADERS = {
+HEADERS1 = {
     'Cookie': 'JSESSIONID=DjXWtvBZ2bZKxsd-72sEwiWrd1IZvCn9SYO0uh3SPt82hEn2JM3W!1722218088'
+}
+HEADERS = {
+    'Cookie': 'JSESSIONID=s67aooqxWDzg6H7zkXGgs-ogShQn9xcp0sbf6IhzKZBOHHyBh9qH!1722218088',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
 }
 CSV_TITLE = ['Document Name', 'Version', 'Size',
              'Update Date', 'Level', 'Subject', 'Document Path']
@@ -54,11 +59,12 @@ class SinglePageDocs():
 
     def __init__(self, url, headers):
         self.__json_res = self.__get_docs(url, headers)
-        self.url = url
+        self.__url = url
+        self.__docs_info = self.__get_docs_info()
 
     def __get_docs(self, url, headers):
         try:
-            res = requests.get(url, headers=headers)
+            res = requests.post(url, headers=headers)
             return res.json()
         except:
             print_err('http_err', url)
@@ -70,15 +76,14 @@ class SinglePageDocs():
         date = datetime.datetime.now().date().strftime('%y%m%d')
         return prefix + date + ext
 
-    def list_to_csv(self):
+    def __get_docs_info(self):
         total = int(self.__json_res['total'])
         rows = self.__json_res['rows']
         if(total != len(rows)):
-            print_err('doc_count_mismatch', self.url)
+            print_err('doc_count_mismatch', self.__url)
             sys.exit(1)
-        f = open(self.__csv_file_name(), "w+", newline='')
-        w = csv.writer(f)
-        w.writerows([CSV_TITLE])
+
+        docs_info = []
         for row in rows:
             name = row['name']
             version = row['version']
@@ -87,19 +92,50 @@ class SinglePageDocs():
             level = row['ipLevel']
             subject = row['subject']
             path = row['description']
+            fileid = row['fileId']
+            docs_info.append((name, version, size, update_date,
+                              level, subject, path, fileid))
 
-            w.writerows(
-                [(name, version, size, update_date, level, subject, path)])
+        return docs_info
+
+    def list_to_csv(self):
+        f = open(self.__csv_file_name(), "w+", newline='')
+        w = csv.writer(f)
+        w.writerows([CSV_TITLE])
+        for doc in self.__docs_info:
+            w.writerows([doc[:7]])
 
         f.close()
         print('[FF] %s' % self.__csv_file_name())
+
+    def __save_a_file(self, doc_info):
+        fileid = doc_info[7]
+        filename = doc_info[0].split('/')[-1]
+        # print(filename)
+        url = 'http://online.hhgrace.com/web/downFile.tdis?id=275672&confidentialLevel=D&userId=2c9e498b48a7386f0148ac31c83f0108&fileId=%s&name=%s' % (
+            fileid, filename)
+        res = requests.get(url, headers=HEADERS, timeout=60)
+        size_of_file = open(filename, 'wb').write(res.content)
+        # kind = filetype.guess(filename)
+        ext = filename.split('.')[-1].lower()
+
+        if ext in ('doc', 'docx'):
+            new_filename = filename.split('.')[0] + '.pdf'
+            shutil.move(filename, new_filename)
+        else:
+            new_filename = filename
+        print('[save] %s (%d bytes)' % (new_filename, size_of_file))
+
+    def save_all_files(self):
+        for doc in self.__docs_info:
+            self.__save_a_file(doc)
 
 
 class DocsTree():
 
     def __init__(self, url, headers):
         self.__top = self.__get_top(url, headers)
-        self.url = url
+        self.__url = url
 
     def __get_top(self, url, headers):
         try:
@@ -107,6 +143,7 @@ class DocsTree():
             return res.json()
         except:
             print_err('http_err', url)
+            print(url)
             sys.exit(1)
 
     def __find_child(self, all_children, child_name):
@@ -155,30 +192,31 @@ def main():
     # sys.stdout = io.TextIOWrapper(
     #     sys.stdout.buffer, encoding='gb18030', line_buffering=True)
     print_version(VERSION)
-    # if len(sys.argv) != 2:
-    #     usage()
-    #     sys.exit(1)
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
 
-    # # path = '0.11um/FS11Q7E8/Design Rule/Platform'
-    # path = sys.argv[1]
-    # docs = DocsTree(URL_TDIS_TOP, HEADERS)
-    # node = docs.node(path)
-    # id = Node(node).getid()
-    # url = get_url(id, path)
+    # path = '0.11um/FS11Q7E8/Design Rule/Platform'
+    path = sys.argv[1]
+    docs = DocsTree(URL_TDIS_TOP, HEADERS)
+    node = docs.node(path)
+    id = Node(node).getid()
+    url = get_url(id, path)
 
-    # a_page_docs = SinglePageDocs(url, HEADERS)
-    # a_page_docs.list_to_csv()
-    # print('\nPress any key to exit ...')
-    # msvcrt.getch()
+    a_page_docs = SinglePageDocs(url, HEADERS)
+    a_page_docs.list_to_csv()
+    a_page_docs.save_all_files()
+    print('\nPress any key to exit ...')
+    msvcrt.getch()
 
-    # url = 'http://online.hhgrace.com/web/downFile.tdis?id=300103&confidentialLevel=D&userId=2c9e498b48a7386f0148ac31c83f0108&fileId=&name=/PDK/release/runset/FAB3/Mentor/0.11um/F011Q7E6/LVS/HG_F011Q7E6_LVS_Cal.cmd'
-    url = 'http://online.hhgrace.com/web/downFile.tdis?id=275672&confidentialLevel=D&userId=2c9e498b48a7386f0148ac31c83f0108&fileId=23178&name=DM-COMM-TLR-1610-0010.docx'
-    res = requests.get(url, headers=HEADERS, timeout=60)
-    file_name = 'a'
-    size_of_file = open(file_name, 'wb').write(res.content)
-    print('[save] %s (%d bytes)' % (file_name, size_of_file))
-    kind = filetype.guess(file_name)
-    print(kind)
+    # # url = 'http://online.hhgrace.com/web/downFile.tdis?id=300103&confidentialLevel=D&userId=2c9e498b48a7386f0148ac31c83f0108&fileId=&name=/PDK/release/runset/FAB3/Mentor/0.11um/F011Q7E6/LVS/HG_F011Q7E6_LVS_Cal.cmd'
+    # url = 'http://online.hhgrace.com/web/downFile.tdis?id=275672&confidentialLevel=D&userId=2c9e498b48a7386f0148ac31c83f0108&fileId=23178&name=DM-COMM-TLR-1610-0010.docx'
+    # res = requests.get(url, headers=HEADERS, timeout=60)
+    # file_name = 'a'
+    # size_of_file = open(file_name, 'wb').write(res.content)
+    # print('[save] %s (%d bytes)' % (file_name, size_of_file))
+    # kind = filetype.guess(file_name)
+    # print(kind)
 
 
 if __name__ == '__main__':
